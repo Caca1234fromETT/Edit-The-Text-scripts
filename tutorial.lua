58d09534697c49412cc47469284f33aa7dcc171f
Caca1234fromETT <108762573+Caca1234fromETT@users.noreply.github.com>
2022-07-05 17:01:33 -0400

    Create tutorial.lua

---
+++ tutorial.lua
@@ -0,0 +1,3 @@
+print("Hello world!") -- prints "Hello world!" in the console
+get("example") -- returns the content of /example
+post("chat", get("chat").."\n\nETTP: automatic reply") -- posts text to /chat
